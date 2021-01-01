1b1c35736ab5aa4648cd21376a6264e5b4232cf6
Sviatoslav <svitoslav@example.com>
2021-01-01 21:41:58 +0200

    add latin first declention test

--- azure_function/athena_bot/bot.py
+++ azure_function/athena_bot/bot.py
@@ -1,7 +1,9 @@
 from telegram import Update
-from telegram.ext import Dispatcher, Updater, CallbackContext, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Dispatcher, Updater, CallbackContext, CommandHandler, CallbackQueryHandler
 import random
 import athena_bot.const as const
+import athena_bot.latin as latin
 
 
 def advice(update: Update, context: CallbackContext) -> None:
@@ -10,12 +12,49 @@ def advice(update: Update, context: CallbackContext) -> None:
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Χαῖρε!")
 
+def test_latin(update: Update, context: CallbackContext) -> None:
+    verbi = [
+        ('puell', latin.Declinatio.Pirma),
+        ('aqu', latin.Declinatio.Pirma),
+        ('grati', latin.Declinatio.Pirma),
+        ('caus', latin.Declinatio.Pirma),
+        ('lingu', latin.Declinatio.Pirma),
+        ('cur', latin.Declinatio.Pirma),
+        ('fam', latin.Declinatio.Pirma),
+        ('rot', latin.Declinatio.Pirma),
+        ('form', latin.Declinatio.Pirma),
+        ('tabul', latin.Declinatio.Pirma),
+        ('fortun', latin.Declinatio.Pirma),
+        ('vi', latin.Declinatio.Pirma),
+        ('glori', latin.Declinatio.Pirma),
+        ('vit', latin.Declinatio.Pirma),
+    ]
+    question = latin.prepare_guestion(random.choice(verbi))
+    keyboard = [
+        [
+            InlineKeyboardButton(question[1][0][0], callback_data=question[1][0][1]),
+            InlineKeyboardButton(question[1][1][0], callback_data=question[1][1][1]),
+        ],
+        [
+            InlineKeyboardButton(question[1][2][0], callback_data=question[1][2][1]),
+            InlineKeyboardButton(question[1][3][0], callback_data=question[1][3][1]),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text(question[0], reply_markup=reply_markup)
+
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer(text=query.data)
 
 handlers = {
     'start': start,
     'advice': advice,
+    'latin': test_latin,
 }
 
 def register_command_handlers(dispatcher : Dispatcher):
     for key, value in handlers.items():
         dispatcher.add_handler(CommandHandler(key, value))
+    dispatcher.add_handler(CallbackQueryHandler(button))
--- start_polling.py
+++ start_polling.py
@@ -1,21 +1,26 @@
 from dotenv import load_dotenv
 import os
 import sys
-import settings
 from telegram.ext import Updater
 
 sys.path.insert(0, "azure_function")
 import athena_bot.bot as athena_bot
+import athena_bot.latin as latin
 
 load_dotenv(verbose=True)
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 def main():
-    """Start the bot."""
     updater = Updater(TOKEN, use_context=True)
     athena_bot.register_command_handlers(updater.dispatcher)
     updater.start_polling()
     updater.idle()
+    # print(latin.conjugate(verbi[0], latin.Numerus.Singularis, latin.Casus.Ablativus))
+    # print(latin.conjugate(verbi[0], latin.Numerus.Pluralis, latin.Casus.Ablativus))
+    # print(verbi[0])
+    #print()
+
+
 
 
 if __name__ == '__main__':
